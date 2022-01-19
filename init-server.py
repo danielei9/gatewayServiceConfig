4ac8850b6040904287b44bc3328b17847e250673
danielei9 <dbs99@hotmail.com>
2022-01-19 21:49:26 +0100

    Update init-server.py

--- init-server.py
+++ init-server.py
@@ -24,4 +24,4 @@ def form_example():
                   <h1>The value is: {}</h1>'''.format(inputIP, inputDIN)
     return "No POST"
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8081, debug=True)
+   app.run(host='0.0.0.0', port=35465, debug=True)
