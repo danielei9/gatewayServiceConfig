f22d009d472f1d61b4a3c3de971a90dcfa0ef7dd
danielei9 <dbs99@hotmail.com>
2022-01-27 15:05:13 +0100

    Update cambiarIp.py

--- cambiarIp.py
+++ cambiarIp.py
@@ -156,7 +156,7 @@ def changeIp(ip, din, inputPEP):
 
     #sudo nano /etc/dhcpcd.conf 
     f.close()
-    os.system('sudo reboot')
+    #os.system('sudo reboot')
     #sudo /etc/init.d/networking restart
 
 
